df9ef0893e3914693e4a1ac121026b51cdc649ae
Belen Garcia <belen@AlejandrosMBP2.fios-router.home>
2020-07-21 18:25:55 -0400

    Removed unnecesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascrip file is loaded!")
-
 // fetch('http://puzzle.mead.io/puzzle').then((response)=> {
 //     response.json().then((data)=>{
 //         console.log(data)
